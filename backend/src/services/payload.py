8f9c2cf5c80d2d121c2a58c7cf248480c857d9e2
imrootedoro <adelsonteodoro67@gmail.com>
2024-08-13 20:15:56 -0300

    feat: configura as variaveis de ambiente

--- backend/src/services/payload.py
+++ backend/src/services/payload.py
@@ -1,9 +1,12 @@
 import requests
+from dotenv import load_dotenv
+import os
 
 
 def send_message_to_dialogflow(mensagem, access_token, user_id):
+    load_dotenv()
     user_id = user_id
-    project_id = 'xadrezbot-vtdd' # variavel que amerzena o id do projeto
+    project_id = os.getenv('project_id') # variavel que amerzena o id do projeto
     url = f"https://dialogflow.googleapis.com/v2/projects/{project_id}/agent/sessions/{user_id}:detectIntent"
     headers = {
         "Authorization": f"Bearer {access_token}",
