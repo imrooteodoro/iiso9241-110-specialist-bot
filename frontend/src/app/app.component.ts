844dcb7676efd725565466c64a8a1e38d531179e
imrooteodoro <adelsonteodoro67@gmail.com>
2024-08-28 20:32:12 -0300

    refactor: refatora  o código do bot componente

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -11,9 +11,104 @@ import { MatButtonModule } from '@angular/material/button';
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, HttpClientModule, CommonModule, FormsModule, MatIconModule, MatButtonModule],
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: `<div class="chat-container" *ngIf="isChatOpen">
+    <div class="chat-history" #chatHistory>
+      <div *ngFor="let message of chat_history" class="chat-message">
+        <div *ngIf="message.usuario" class="user-message">{{ message.usuario }}</div>
+        <div *ngIf="message.bot" class="bot-message" [innerHTML]="message.bot"></div>
+      </div>
+    </div>
+    <div class="chat-input">
+      <input type="text" [(ngModel)]="mensagem" placeholder="Digite sua mensagem" (keyup.enter)="sendMessage()">
+      <button (click)="sendMessage()">Enviar</button>
+    </div>
+  </div>
+  
+  <button mat-fab class="fab-button" (click)="toggleChat()">
+    <mat-icon>chat</mat-icon>
+  </button>
+  `,
+  styles: `.fab-button {
+    position: fixed;
+    bottom: 16px;
+    right: 16px;
+    color: green;
+    background-color: black;
+  }
+  
+
+.chat-container {
+    position: fixed;
+    bottom: 20px;
+    right: 20px;
+    width: 400px;
+    height: 500px;
+    background-color: #fff;
+    border-radius: 10px;
+    box-shadow: 0 5px 15px rgba(0, 0, 0, 0.1);
+    display: flex;
+    flex-direction: column;
+  
+    .chat-history {
+      flex-grow: 1;
+      overflow-y: auto;
+      padding: 20px;
+  
+      .chat-message {
+        margin-bottom: 10px;
+  
+        .user-message {
+          background-color: #dcf8c6;
+          border-radius: 15px;
+          padding: 8px 12px;
+          display: inline-block;
+          max-width: 70%;
+          text-align: left;
+          word-wrap: break-word;
+        }
+  
+        .bot-message {
+          background-color: #f1f0f0;
+          border-radius: 15px;
+          padding: 8px 12px;
+          display: inline-block;
+          max-width: 70%;
+          text-align: left;
+          word-wrap: break-word;
+        }
+      }
+    }
+  
+    .chat-input {
+      display: flex;
+      align-items: center;
+      padding: 10px 20px;
+      border-top: 1px solid #e6e6e6;
+  
+      input {
+        flex-grow: 1;
+        padding: 8px 12px;
+        border: 1px solid #ccc;
+        border-radius: 15px;
+        font-size: 14px;
+        outline: none;
+      }
+  
+      button {
+        margin-left: 10px;
+        padding: 8px 16px;
+        background-color: #4caf50;
+        color: #fff;
+        border: none;
+        border-radius: 15px;
+        font-size: 14px;
+        cursor: pointer;
+      }
+    }
+  }`
 })
+
+
 export class AppComponent implements OnInit, AfterViewChecked {
   mensagem: string = '';
   bot_messageHtml: SafeHtml | null = null;
@@ -41,7 +136,7 @@ export class AppComponent implements OnInit, AfterViewChecked {
     this.chat_history.push({ usuario: this.mensagem, bot: null });
 
     const requestBody = { mensagem: this.mensagem };
-    this.http.post('http://10.0.0.84:5000/dialogflow', requestBody)
+    this.http.post('http://10.113.60.211:5000/dialogflow', requestBody)
       .subscribe(
         (resp: any) => {
           // Converter a mensagem do bot em HTML seguro
